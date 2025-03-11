eedfcd5378cd7451bf6c457362a54840ecdcd6b0
Gillich Norbert <gillich.norbert@icloud.com>
2025-03-11 15:58:45 +0100

    Finomítások

--- Frontend/InstantRecipe/src/app/recipes/recipes.component.ts
+++ Frontend/InstantRecipe/src/app/recipes/recipes.component.ts
@@ -3,25 +3,39 @@ import { RecipeService } from '../recipe.service';
 import { Recipe } from '../models/recipe.model';
 import AOS from 'aos';
 import { Ingredient } from '../models/ingredient.model';
+import { ConfigService } from '../config.service';
 
 @Component({
   selector: 'app-recipes',
   templateUrl: './recipes.component.html',
   styleUrls: ['./recipes.component.css']
 })
-export class RecipesComponent implements OnInit {
+export class RecipesComponent {
   @Input() filteredRecipes: Recipe[] = [];
   recipes: Recipe[] = [];
   ingredients: Ingredient[] = [];
+  ingredientsHeaderTitle = "Ingredients";
+  actLang = 'Magyar';
+  langSign = ""
   
 
-  constructor(private recipeService: RecipeService) {}
+  constructor(private recipeService: RecipeService, private config: ConfigService) {
+    this.loadContent()
+    this.langSign = config.langSign 
+  }
 
   ngOnInit(): void {
     AOS.init({
       once: true
     });
+    this.loadContent()
 
+  }
+
+  loadContent() {
+    this.config.getContent().subscribe((content) => {
+      this.ingredientsHeaderTitle = content.ingredientsHeaderTitle
+    });
   }
 
   getIngredients(ingredient: Ingredient): string[] {
